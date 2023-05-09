6dfadb600deefae684170cc9aacb020459752169
Théo FASSI <tfa@subteno.com>
2023-05-09 11:09:47 +0200

    imp: error handling, types

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,6 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+import type Participation from './Participation';
+export default interface Olympic {
+  id: number;
+  country: string;
+  participations: Participation[];
 }
-*/
--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,35 @@
+import type Olympic from '../models/Olympic';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[]>([]);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
-      })
+      catchError(this._handleError)
     );
   }
 
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+
+  private _handleError<T>(error: any, caught: T): T {
+    this.olympics$.unsubscribe();
+    this.router.navigate(['not-found']);
+
+    return caught;
   }
 }
